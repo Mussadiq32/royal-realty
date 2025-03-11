c6d1232bae29bd2e01155146a8948600881d402c
Mussadiq32 <120275986+Mussadiq32@users.noreply.github.com>
2025-03-11 23:20:26 +0530

    files

--- src/pages/services/InvestmentService.tsx
+++ src/pages/services/InvestmentService.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 import Navbar from '../../components/layout/Navbar';
 import Footer from '../../components/layout/Footer';
 import { Mail, Phone, ArrowRight, BarChart, TrendingUp, Shield, Check } from 'lucide-react';
-import { Button } from '@/components/ui/button';
+import { Button } from '@/components/ui/Button';
 
 const InvestmentService = () => {
   useEffect(() => {
